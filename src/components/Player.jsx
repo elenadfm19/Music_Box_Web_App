5044662d5553d103d15e5b546a23369f221fb9f2
elenadfm19 <elenadefrias@gmail.com>
2026-01-18 22:17:44 +0000

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styles from './Player.module.css';
-const key = import.meta.env.VITE_YOUTUBE_KEY;
+const key = import.meta.env.VITE_GOOGLEAPIS_KEY;
 
 function Player({ track }) {
   const [videoId, setVideoId] = useState("");
