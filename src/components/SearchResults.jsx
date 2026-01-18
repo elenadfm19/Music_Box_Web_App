5b49ae40f40ebcf62ff49a87117a19e8969e8775
elenadfm19 <elenadefrias@gmail.com>
2026-01-18 21:54:39 +0000

    Update SearchResults.jsx

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import styles from "./SearchResults.module.css";
 import Track from "./Track.jsx";
+import { getTrackThumbnail } from "../helpers/getTrackThumbnail.js";
 
 function SearchResults({ searchList, playList, setPlayList }) {
+
+
   function handleOnClick(e) {
     const id = e.target.id;
     const tracks = [...playList, searchList[id]];
     setPlayList(tracks);
   }
+  console.log("entro a searchresults");
+  console.log(searchList);
 
   return (
     <>
@@ -16,9 +21,13 @@ function SearchResults({ searchList, playList, setPlayList }) {
         {searchList.map((track, index) => (
           <div className={styles.track}>
             <p className={styles.add} onClick={handleOnClick} id={index}>
-               + Add to Playlist 
+              + Add to Playlist
             </p>
-            <Track track={track} index={index} />
+            <Track
+              track={track}
+              thumbnail={getTrackThumbnail(track)}
+              index={index}
+            />
           </div>
         ))}
       </div>
